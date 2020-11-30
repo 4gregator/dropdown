09f1a45ddcd17f70fc7d8b5b390f984895131259
Agregat <ikrasnobai@yandex.ru>
2020-11-30 14:57:15 +0300

    Update Dropdown.js

--- assets/js/Dropdown.js
+++ assets/js/Dropdown.js
@@ -142,7 +142,6 @@ class Dropdown {
       }
     });
     this.drop.addEventListener('keyup', function (e) {
-      console.log(e.keyCode);
       if (e.keyCode == 27) {
         // esc
         self.loseFocus();
